339f9c22a47aa4f2869bbf92221a5d95b2941403
lmadera1 <luismmadera.lm@gmail.com>
2024-09-18 03:58:06 -0700

    Added Valedation Layers for debugging. Added code to select and store physical device. Added check to devices for Queue Family support.

--- GraphicsPractice2/main.cpp
+++ GraphicsPractice2/main.cpp
@@ -4,7 +4,10 @@
 //#include <stdexcept>
 
 #include <iostream>
+#include <vector>
+#include <optional>
 
+//Window size constants
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
@@ -17,17 +20,42 @@ public:
         cleanup();
     }
 private:
+    //VulkanApp constants
     GLFWwindow* window;
     VkInstance instance;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
+    //VulkanApp Validation Layers
+    const std::vector<const char*> validationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+    };
+
+    //Lookup for Queue Families
+    struct QueueFamilyIndices {
+        std::optional<uint32_t> graphicsFamily;
+
+        bool isComplete() {
+            return graphicsFamily.has_value();
+        }
+    };
+
+
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
 
     void initWindow() {
         glfwInit();
-
+        //TODO: Make GLfW Window resizeable
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
+
     void initVulkan() {
         createInstance();
+        pickPhysicalDevice();
     }
 
     void mainLoop() {
@@ -45,6 +73,11 @@ private:
     }
 
     void createInstance() {
+        if (enableValidationLayers && !checkValidationLayerSupport()) {
+            throw std::runtime_error("validation layers requested, but not available!");
+        }
+
+        //Vulkan App info
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
         appInfo.pApplicationName = "GraphicsProject2";
@@ -53,23 +86,134 @@ private:
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.apiVersion = VK_API_VERSION_1_0;
 
+        //CreateInfo for Instance
         VkInstanceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
 
+        //Get Required Instance Extensions
         uint32_t glfwExtensionCount = 0;
         const char** glfwExtensions;
-
         glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
         createInfo.enabledExtensionCount = glfwExtensionCount;
         createInfo.ppEnabledExtensionNames = glfwExtensions;
-        //TODO: Change line below
-        createInfo.enabledLayerCount = 0;
 
+        //Enable Validation Layers for debug
+        if (enableValidationLayers) {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        } else {
+            createInfo.enabledLayerCount = 0;
+        }
+
+        //Try creating Instance
         if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
             throw std::runtime_error("failed to create instance!");
         }
 
+    }
+
+    bool checkValidationLayerSupport() {
+        //Get Layer Count
+        uint32_t layerCount;
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+        //Put Layers into vector
+        std::vector<VkLayerProperties> availableLayers(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+        //Check if requested Validation Layers are available
+        for (const auto& layerName : validationLayers) {
+            bool layerFound = false;
+
+            for (const auto& layerProperties : availableLayers) {
+                if (strcmp(layerName, layerProperties.layerName) == 0) {
+                    layerFound = true;
+                    break;
+                }
+            }
+            //Validation Layer not available
+            if (!layerFound) {
+                return false;
+            }
+        }
+        //All Valedation Layers available
+        return true;
+    }
+
+    void pickPhysicalDevice() {
+        //Get number of devices
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+        //No Vulkan supported devices found
+        if (deviceCount == 0) {
+            throw std::runtime_error("failed to find GPUs with Vulkan support!");
+        }
+
+        //Get devices
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+        //Pick first suitable device
+        for (VkPhysicalDevice& device : devices) {
+            if (isValidDevice(device)) {
+                physicalDevice = device;
+                break;
+            }
+        }
+
+        //physicalDevice not properlly set
+        if (physicalDevice == VK_NULL_HANDLE) {
+            throw std::runtime_error("failed to find a suitable GPU!");
+        }
+
+        //Print out chosen device's name to std output
+        VkPhysicalDeviceProperties deviceProperties;
+        vkGetPhysicalDeviceProperties(physicalDevice, &deviceProperties);
+        std::cout << "GPU: " << deviceProperties.deviceName << std::endl;
+    }
+
+    bool isValidDevice(VkPhysicalDevice& device) {
+        //Get Queue Families
+        QueueFamilyIndices indices = findQueueFamilies(device);
+
+        //Device doesn't support proper Queue Families
+        if (!indices.isComplete()) return false;
+
+        //Get device properties
+        VkPhysicalDeviceProperties deviceProperties;
+        vkGetPhysicalDeviceProperties(device, &deviceProperties);
+        
+        //TODO: Change this to something else
+        return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU;
+    }
+
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice& device) {
+        QueueFamilyIndices indices;
+
+        //Get Queue Family Count
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+        //Get vector of Queue Families
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+        int i = 0;
+        for (const auto& queueFamily : queueFamilies) {
+            if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+                indices.graphicsFamily = i;
+            }
+
+            if (indices.isComplete()) {
+                break;
+            }
+
+            i++;
+        }
+
+        return indices;
     }
 };
 
